44b516e32960804bc3532f13a98320c13eee0cfc
Panzo98 <49492024+Panzo98@users.noreply.github.com>
2020-10-27 11:50:10 +0100

    Login form

---
+++ restoranApp/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Dim rd As SqlDataReader
+
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Riki\Desktop\imenikTelefonski\imenikTelefonski\aperionBaza.mdf;Integrated Security=True"
+        cmd.Connection = con
+        con.Open()
+        cmd.CommandText = " select username, pass from korisnici where username = '" & textBox1.Text & "' and pass = '" & textBox2.Text & "'"
+
+        rd = cmd.ExecuteReader
+
+        If rd.HasRows Then
+
+            prozor.Show()
+            Me.Hide()
+            textBox1.Text = "Korisničko ime"
+            textBox2.Text = "Lozinka"
+        Else
+            MessageBox.Show("Neuspješna prijava!")
+        End If
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub textBox1_Enter(sender As Object, e As EventArgs) Handles textBox1.Enter
+        If textBox1.Text = "Korisničko ime" Then
+            textBox1.Text = ""
+        End If
+    End Sub
+    Private Sub textBox1_Leave(sender As Object, e As EventArgs) Handles textBox1.Leave
+        If textBox1.Text = "" Then
+            textBox1.Text = "Korisničko ime"
+        End If
+    End Sub
+
+
+    Private Sub textBox2_Enter(sender As Object, e As EventArgs) Handles textBox2.Enter
+        If textBox2.Text = "Lozinka" Then
+            textBox2.Text = ""
+            textBox2.PasswordChar = "*"
+        End If
+    End Sub
+    Private Sub textBox2_Leave(sender As Object, e As EventArgs) Handles textBox2.Leave
+        If textBox2.Text = "" Then
+            textBox2.Text = "Lozinka"
+            textBox2.PasswordChar = Nothing
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        button2.Select()
+    End Sub
+End Class
---
+++ restoranApp/prijava.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class prijava
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(prijava))
+        Me.pictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.pictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.panel3 = New System.Windows.Forms.Panel()
+        Me.textBox2 = New System.Windows.Forms.TextBox()
+        Me.panel4 = New System.Windows.Forms.Panel()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.pictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.pictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.pictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pictureBox4
+        '
+        Me.pictureBox4.Image = CType(resources.GetObject("pictureBox4.Image"), System.Drawing.Image)
+        Me.pictureBox4.Location = New System.Drawing.Point(436, 318)
+        Me.pictureBox4.Name = "pictureBox4"
+        Me.pictureBox4.Size = New System.Drawing.Size(35, 38)
+        Me.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pictureBox4.TabIndex = 44
+        Me.pictureBox4.TabStop = False
+        '
+        'pictureBox3
+        '
+        Me.pictureBox3.Image = CType(resources.GetObject("pictureBox3.Image"), System.Drawing.Image)
+        Me.pictureBox3.Location = New System.Drawing.Point(436, 247)
+        Me.pictureBox3.Name = "pictureBox3"
+        Me.pictureBox3.Size = New System.Drawing.Size(35, 38)
+        Me.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pictureBox3.TabIndex = 43
+        Me.pictureBox3.TabStop = False
+        '
+        'button2
+        '
+        Me.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button2.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button2.Location = New System.Drawing.Point(436, 459)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(278, 48)
+        Me.button2.TabIndex = 42
+        Me.button2.Text = "IZLAZ"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button1.Location = New System.Drawing.Point(436, 395)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(278, 48)
+        Me.button1.TabIndex = 41
+        Me.button1.Text = "PRIJAVI SE"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'panel3
+        '
+        Me.panel3.BackColor = System.Drawing.Color.Black
+        Me.panel3.Location = New System.Drawing.Point(490, 355)
+        Me.panel3.Name = "panel3"
+        Me.panel3.Size = New System.Drawing.Size(224, 1)
+        Me.panel3.TabIndex = 40
+        '
+        'textBox2
+        '
+        Me.textBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.textBox2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textBox2.Location = New System.Drawing.Point(490, 330)
+        Me.textBox2.Name = "textBox2"
+        Me.textBox2.Size = New System.Drawing.Size(224, 19)
+        Me.textBox2.TabIndex = 39
+        Me.textBox2.Text = "Lozinka"
+        '
+        'panel4
+        '
+        Me.panel4.BackColor = System.Drawing.Color.Black
+        Me.panel4.Location = New System.Drawing.Point(490, 284)
+        Me.panel4.Name = "panel4"
+        Me.panel4.Size = New System.Drawing.Size(224, 1)
+        Me.panel4.TabIndex = 38
+        '
+        'textBox1
+        '
+        Me.textBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.textBox1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textBox1.Location = New System.Drawing.Point(490, 259)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(224, 19)
+        Me.textBox1.TabIndex = 37
+        Me.textBox1.Text = "Korisničko ime"
+        '
+        'pictureBox2
+        '
+        Me.pictureBox2.Image = CType(resources.GetObject("pictureBox2.Image"), System.Drawing.Image)
+        Me.pictureBox2.Location = New System.Drawing.Point(436, 12)
+        Me.pictureBox2.Name = "pictureBox2"
+        Me.pictureBox2.Size = New System.Drawing.Size(278, 200)
+        Me.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pictureBox2.TabIndex = 36
+        Me.pictureBox2.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Panel1.Controls.Add(Me.pictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(394, 537)
+        Me.Panel1.TabIndex = 35
+        '
+        'pictureBox1
+        '
+        Me.pictureBox1.Image = CType(resources.GetObject("pictureBox1.Image"), System.Drawing.Image)
+        Me.pictureBox1.Location = New System.Drawing.Point(48, 193)
+        Me.pictureBox1.Name = "pictureBox1"
+        Me.pictureBox1.Size = New System.Drawing.Size(296, 160)
+        Me.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pictureBox1.TabIndex = 2
+        Me.pictureBox1.TabStop = False
+        '
+        'prijava
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 537)
+        Me.Controls.Add(Me.pictureBox4)
+        Me.Controls.Add(Me.pictureBox3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.panel3)
+        Me.Controls.Add(Me.textBox2)
+        Me.Controls.Add(Me.panel4)
+        Me.Controls.Add(Me.textBox1)
+        Me.Controls.Add(Me.pictureBox2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "prijava"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "prijava"
+        CType(Me.pictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents pictureBox4 As PictureBox
+    Private WithEvents pictureBox3 As PictureBox
+    Private WithEvents button2 As Button
+    Private WithEvents button1 As Button
+    Private WithEvents panel3 As Panel
+    Private WithEvents textBox2 As TextBox
+    Private WithEvents panel4 As Panel
+    Private WithEvents textBox1 As TextBox
+    Private WithEvents pictureBox2 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Private WithEvents pictureBox1 As PictureBox
+End Class
