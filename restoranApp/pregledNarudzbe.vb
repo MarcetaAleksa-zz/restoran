79284bd9776fc8516e81f64b6e93878f6612566b
Panzo98 <49492024+Panzo98@users.noreply.github.com>
2020-10-28 21:48:09 +0100

    Update v2.0 Izlistavanje narudzbi i sortiranje

--- restoranApp/pregledNarudzbe.vb
+++ restoranApp/pregledNarudzbe.vb
@@ -15,70 +15,87 @@ Public Class pregledNarudzbe
         Dim sqlCommand As New SqlCommand("SELECT * FROM narudzbak ", Baza.connection)
         Dim adapter As New SqlDataAdapter(sqlCommand)
         Dim oprema_table1 As New DataTable()
-        Dim brojacopreme As Integer
         adapter.Fill(oprema_table1)
-        brojacopreme = oprema_table1.Rows.Count
-        Try
-            ' Label4.Text = oprema_table1.Rows(i)(2)
-            ' Label5.Text = oprema_table1.Rows(i)(3)
-            ' Label6.Text = oprema_table1.Rows(i)(4)
-            Dim k As Integer = 2
-            Do While k <> 6
-                If oprema_table1(i)(k) <> 0 Then
+
+        Dim sqlCommand1 As New SqlCommand("SELECT * FROM Skladiste ", Baza.connection)
+        Dim adapter1 As New SqlDataAdapter(sqlCommand1)
+        Dim oprema_table11 As New DataTable()
+        adapter1.Fill(oprema_table11)
+        Dim brojacOpreme As Integer = 0
+        brojacOpreme = oprema_table11.Rows.Count
+        Dim nesto As Integer = 0
+        Dim noviRed As Integer = 0
+        Label3.Text = oprema_table1.Rows(i)(0)
+        Label4.Text = oprema_table1.Rows(i)(1)
+        For k = 2 To brojacOpreme + 1
+            Try
+                nesto = k - 2
+
+                If oprema_table1(i)(k) <> 0 And oprema_table1(i)(k) <> Nothing Then
                     Dim L As Label = New Label
                     With L
-                        .Text = 'oprema_table1(i)(2)             'naziv artikla izvucen u label
+                        .Text = oprema_table11(nesto)(0)             'naziv artikla izvucen u label
                         .TextAlign = ContentAlignment.MiddleCenter
                         .Visible = True
                         .Font = New Font("Microsoft Sans Serif", 14)
                         .BackColor = Color.Transparent
                         .Dock = DockStyle.Fill
-                        tab1.Controls.Add(L, 0, i)
+                        tab1.Controls.Add(L, 0, k - 2)
                     End With
-                    k = +1
+
+                    Dim L1 As Label = New Label
+                    With L1
+                        .Text = oprema_table1(i)(k)
+                        .Name = "L1" + i.ToString 'kolicina narucenog izvucena u label
+                        .TextAlign = ContentAlignment.MiddleCenter
+                        .Visible = True
+                        .Font = New Font("Microsoft Sans Serif", 14)
+                        .BackColor = Color.Transparent
+                        .Dock = DockStyle.Fill
+                        tab1.Controls.Add(L1, 1, k - 2)
+                    End With
+
+                    Dim L2 As Label = New Label
+                    With L2
+                        .Text = Math.Round(oprema_table11(nesto)(2), 2)
+                        .TextAlign = ContentAlignment.MiddleCenter 'cijena izvucena u label
+                        .Visible = True
+                        .BackColor = Color.Transparent
+                        .Font = New Font("Microsoft Sans Serif", 14)
+                        .Dock = DockStyle.Fill
+                        tab1.Controls.Add(L2, 2, k - 2)
+                    End With
+                    Try
+                        Dim L3 As Label = New Label
+                        With L3
+                            .Text = Math.Round(L2.Text * CDec(Val(L1.Text)), 2)
+                            'Math.Round((L2.Text * L1.Text), 2)
+                            .TextAlign = ContentAlignment.MiddleCenter 'cijena izvucena u label
+                            .Visible = True
+                            .BackColor = Color.Transparent
+                            .Font = New Font("Microsoft Sans Serif", 14)
+                            .Dock = DockStyle.Fill
+                            tab1.Controls.Add(L3, 3, k - 2)
+                        End With
+                        Label12.Text = Math.Round(CDec(Val(Label12.Text)) + CDec(Val(L3.Text)), 2) & "KM"
+                    Catch ex As Exception
+                        Dim L3 As Label = New Label
+                        With L3
+                            .Text = "error 404"
+                            .TextAlign = ContentAlignment.MiddleCenter
+                            .Visible = True
+                            .BackColor = Color.Transparent
+                            .Font = New Font("Microsoft Sans Serif", 14)
+                            .Dock = DockStyle.Fill
+                            tab1.Controls.Add(L3, 3, k - 2)
+                        End With
+                    End Try
+
+                    tab1.HorizontalScroll.Enabled = False
                 End If
-
-
-
-                Dim L1 As Label = New Label
-                With L1
-                    .Text = oprema_table1(i)(1)
-                    .Name = "L1" + i.ToString 'kolicina izvucena u label
-                    .TextAlign = ContentAlignment.MiddleCenter
-                    .Visible = True
-                    .Font = New Font("Microsoft Sans Serif", 14)
-                    .BackColor = Color.Transparent
-                    .Dock = DockStyle.Fill
-                    tab1.Controls.Add(L1, 1, i)
-                End With
-                k = +1
-
-                Dim L2 As Label = New Label
-                With L2
-                    .Text = oprema_table1(i)(2)
-                    .TextAlign = ContentAlignment.MiddleCenter 'cijena izvucena u label
-                    .Visible = True
-                    .BackColor = Color.Transparent
-                    .Font = New Font("Microsoft Sans Serif", 14)
-                    .Dock = DockStyle.Fill
-                    tab1.Controls.Add(L2, 2, i)
-                End With
-                k = +1
-
-                Dim L3 As Label = New Label
-                With L3
-
-                    .Text = "OTVORI"
-                    .Name = "b" + i.ToString
-                    .Visible = True
-                    .Size = New Size(100, 40)
-                    tab1.Controls.Add(L3, 3, i)
-                End With
-                k = +1
-            Loop
-            tab1.HorizontalScroll.Enabled = False
-        Catch ex As Exception
-        End Try
+            Catch ex As Exception
+            End Try
+        Next
 
     End Sub
 
@@ -87,6 +104,9 @@ Public Class pregledNarudzbe
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Controls.Clear() 'removes all the controls on the form
+        InitializeComponent() 'load all the controls again
+        pregledNarudzbe_Load(e, e)
         Me.Hide()
         sanker.Enabled = True
         sanker.Focus()
